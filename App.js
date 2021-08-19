b05b7da11b48f6809d1eaa3a151332ab8dd2c636
George <georg.rashkov@gmail.com>
2021-08-19 23:46:12 +0300

    update

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import Navigator from './router';
 
 export default function App() {
   return (
